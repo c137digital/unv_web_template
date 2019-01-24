1523bcd0059c5ed50e48da4a81bc83235df2ed63
Morty Space <morty.space@gmail.com>
2019-01-24 22:00:15 +0200

    Improved vagrant setup

--- src/app/components/deploy/tasks/app.py
+++ src/app/components/deploy/tasks/app.py
@@ -1,12 +1,13 @@
 from pathlib import Path
 
 from unv.deploy.helpers import (
-    task, run, create_user, copy_ssh_key_for_user, as_user
+    task, run, create_user, copy_ssh_key_for_user, as_user, sync_dir
 )
 from unv.deploy.packages import python
 from unv.deploy.settings import SETTINGS
 
 
+@as_user('vagrant')
 def setup_vagrant():
     run('sudo passwd root')
     copy_ssh_key_for_user('root', Path(SETTINGS['keys']['public']))
@@ -14,16 +15,17 @@ def setup_vagrant():
 
 @task
 def setup():
+    setup_vagrant()
     user = SETTINGS['components']['app']['user']
-    as_user('vagrant', setup_vagrant)()
     create_user(user)
     copy_ssh_key_for_user(user, Path(SETTINGS['keys']['public']))
     python.build(Path('/home/', user, 'python'))
 
 
-# @task
-# def sync():
-    # sync_dir()
+@task
+def sync():
+    sync_dir('src', 'app')
+
 
 
 # @task
