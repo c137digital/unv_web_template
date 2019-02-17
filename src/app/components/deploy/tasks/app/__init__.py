b4a58e2cd74930dcd584f7e4225ef43039158c6d
Morty Space <morty.space@gmail.com>
2019-02-17 22:43:03 +0200

    Moved vagrant operations to unv.deploy

--- src/app/components/deploy/tasks/app/__init__.py
+++ src/app/components/deploy/tasks/app/__init__.py
@@ -1,35 +1,24 @@
 from pathlib import Path
 
 from unv.deploy.helpers import (
-    task, run, create_user, copy_ssh_key_for_user, as_user, sync_dir, local,
-    put, mkdir
+    task, run, create_user, as_user, sync_dir, put, mkdir,
+    copy_ssh_key_for_user
 )
-from unv.deploy.packages import PythonPackage
+from unv.deploy.packages import PythonPackage, VagrantPackage
 from unv.deploy.settings import SETTINGS as DEPLOY
 
 
 APP = DEPLOY['components']['app']
 
 python = PythonPackage(__file__, APP.get('python', {}))
+vagrant = VagrantPackage(__file__, DEPLOY)
 as_app = as_user('app')
-
-
-@as_user('vagrant')
-def setup_vagrant():
-    local('vagrant destroy -f')
-    local('vagrant up')
-
-    # verify server so no timeout on next connection
-    local('vagrant ssh -c "sleep 0.1"')
-
-    copy_ssh_key_for_user('root', Path(DEPLOY['keys']['public']))
 
 
 @task
 @as_app
 def setup():
-    setup_vagrant()
-
+    vagrant.setup()
     create_user(APP['user'])
     copy_ssh_key_for_user(APP['user'], Path(DEPLOY['keys']['public']))
 
